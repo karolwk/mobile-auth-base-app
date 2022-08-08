266c4b8280b3b9ff886e70b9d8b5016adf2ef49d
karolwk <krl@o2.pl>
2022-08-08 15:54:20 +0200

    code refactor

--- util/auth.js
+++ util/auth.js
@@ -1,14 +1,24 @@
 import axios from 'axios';
 
 const API_KEY = 'AIzaSyB61hgmFHXsFXBskKEKIaNG1UypKdTSsGw';
+export const authenticate = async (mode, email, password) => {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+  console.log(url);
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
 
-export const createUser = async (email, password) => {
-  const response = await axios.post(
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    }
-  );
+  // console.log(response.data);
+  const token = response.data.idToken;
+  return token;
+};
+
+export const createUser = (email, password) => {
+  return authenticate('signUp', email, password);
+};
+
+export const login = (email, password) => {
+  return authenticate('signInWithPassword', email, password);
 };
